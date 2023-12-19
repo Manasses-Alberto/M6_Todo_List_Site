2a65d71dcaa24af0c77cab9d9cd22c8c638a2393
Manasses-Alberto <manassesndombele88@gmail.com>
2023-12-19 10:01:59 +0100

    Fiz alguns scripts adicionais para o template index.html

--- m6_todo_app/static/scripts/index.js
+++ m6_todo_app/static/scripts/index.js
@@ -1,7 +1,37 @@
-document.getElementById('task-search-btn').addEventListener('click', () => {
-    document.getElementById('task-search').focus()
-})
-
 document.getElementById('save-task-btn').addEventListener('click', () => {
     document.getElementById('submit-new-task').click()
+})
+
+const nav_links = document.getElementsByClassName('nav-link')
+nav_links[0].addEventListener('click', () => {
+    if (nav_links[0].classList.contains('active') == false) {
+        nav_links[0].classList.add('active')
+        nav_links[1].classList.remove('active')
+        nav_links[2].classList.remove('active')
+        document.getElementsByClassName('tasks-display')[0].style.display = 'block'
+        document.getElementsByClassName('tasks-display')[1].style.display = 'none'
+        document.getElementsByClassName('tasks-display')[2].style.display = 'none'
+    }
+})
+
+nav_links[1].addEventListener('click', () => {
+    if (nav_links[1].classList.contains('active') == false) {
+        nav_links[1].classList.add('active')
+        nav_links[0].classList.remove('active')
+        nav_links[2].classList.remove('active')
+        document.getElementsByClassName('tasks-display')[1].style.display = 'block'
+        document.getElementsByClassName('tasks-display')[0].style.display = 'none'
+        document.getElementsByClassName('tasks-display')[2].style.display = 'none'
+    }
+})
+
+nav_links[2].addEventListener('click', () => {
+    if (nav_links[2].classList.contains('active') == false) {
+        nav_links[2].classList.add('active')
+        nav_links[0].classList.remove('active')
+        nav_links[1].classList.remove('active')
+        document.getElementsByClassName('tasks-display')[2].style.display = 'block'
+        document.getElementsByClassName('tasks-display')[0].style.display = 'none'
+        document.getElementsByClassName('tasks-display')[1].style.display = 'none'
+    }
 })
